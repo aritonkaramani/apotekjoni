55933aa4b9461eb2dfa629107c181579bdc86db2
Ariton Karamani <ariton.karamani@gmail.com>
2021-12-01 16:11:16 +0100

    fixes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,15 +7,13 @@ export default function Intro() {
     return (
         <div className="entrance" id="intro">
             <div className="logo">
-            <LocalHospitalSharpIcon className="plusIkon"/> <h2>Apotek Elinegård</h2>
+                <LocalHospitalSharpIcon className="plusIkon"/> 
+                <h2>Apotek Elinegård</h2>
             
             
             </div> 
-            <div className="titletext">
-            <p>Planerad öppning för Apotek Elinegård är oktober 2021</p>
-            </div>
             <a href="#services">
-                    <ExpandMoreIcon className="icon"/>
+                <ExpandMoreIcon className="icon"/>
             </a>
         </div>
     )
